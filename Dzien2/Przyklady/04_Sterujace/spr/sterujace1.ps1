6fae26a4062709a7750943c2a7b4a83a6e8a2de5
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2025-04-08 11:50:53 +0200

    sterujace

---
+++ Dzien2/Przyklady/04_Sterujace/spr/sterujace1.ps1
@@ -0,0 +1,24 @@
+﻿for(;;)
+{
+clear-host
+$choice=Read-Host -Prompt "Co wybierasz ?"
+Switch ($choice) {
+    {$_ -lt 2 }  { Write-Host “Liczba $choice jest mniejsza od 2” }
+    {$_ -lt 4 }  { Write-Host “Liczba $choice jest mniejsza od 4” }
+    {$_ -lt 6 }  { Write-Host “Liczba $choice jest mniejsza od 6” }
+    Default { Write-Host “INNA” }
+}
+$x=read-host -Prompt "x konczy zabawę"
+if ($x -eq "x"){break}
+}
+
+
+
+$ip=Read-Host -Prompt "Podaj adres IP ?"
+Switch -WildCard ($ip) {
+“192.168*” { Write-Host “To jest adres prywatny w klasie C” }
+“172.16*"  { Write-Host “To jest adres prywatny w klasie B” }
+“172.17*"  { Write-Host “To jest adres prywatny w klasie B” }
+“10*”      { Write-Host “To jest adres prywatny w klasie A” }
+Default { Write-Host “To jest adres INNY” }
+}
